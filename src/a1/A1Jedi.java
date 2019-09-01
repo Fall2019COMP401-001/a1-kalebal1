a53c1137da60944f9e2cb8fbcbbe871bf2bc2a3e
kaleb <kaleb@kalebs-air.lan1>
2019-08-31 20:57:57 -0400

    removed debugging print statements

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -46,18 +46,17 @@ public class A1Jedi {
 			
 			// adds names to separate arrays 
 			firstNames[i] = scan.next();
-			System.out.println(firstNames[i]);
+		
 			lastNames[i] = scan.next();
 			
 			//creates counter for number of different items customer bought
 			int cItems = scan.nextInt();
-			System.out.println("cItems = " + cItems);
+		
 			
 			// loops through number of customer items 
 			for(int j = 0; j < cItems; j++) {
 				int localQuan = scan.nextInt();
 				String itemName = scan.next();
-				System.out.println("itemName = " + itemName);
 				
 				
 				itemsBought [findPlace(itemName, items)][i] += localQuan;
